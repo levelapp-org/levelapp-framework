bedd7cc33c4c8f89ebb568398df34b5757b1ed42
KadriSof <kadrisofyen@gmail.com>
2025-11-13 18:13:56 +0100

    retriever encoding hotfix

--- levelapp/assessor/strategies/baseline/retriever.py
+++ levelapp/assessor/strategies/baseline/retriever.py
@@ -3,6 +3,8 @@ import numpy as np
 
 from typing import List, Dict, Any
 
+from sentence_transformers import SentenceTransformer
+
 from levelapp.assessor.schemas import Document
 from levelapp.assessor.registry import RetrievalStrategy
 
@@ -11,7 +13,8 @@ class BaselineRetrievalStrategy(RetrievalStrategy):
     """Retrieve top-k most relevant chunks by cosine similarity."""
 
     async def run(self, query: str, embedded_chunks: List[Dict[str, Any]]) -> List[Document]:
-        query_vec = self.config.get("embedder").encode([query], normalize_embeddings=True)[0]
+        model = SentenceTransformer(self.config.get("embedder", "sentence-transformers/all-MiniLM-L6-v2"))
+        query_vec = np.array(model.encode(query))
 
         # TODO-0: For the sake of not going insane, create some DTO to ensure that the data is transferred properly
         # TODO-0: between the different levels.
